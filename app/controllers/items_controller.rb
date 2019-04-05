90381c85200930e3cf6c38a04a5f5105e43aafea
narimiya.ryunosuke <gpz9ra4@icloud.com>
2019-04-05 10:28:36 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  
   def new
     @items = []
 
@@ -11,11 +13,17 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+
   
   private
 
